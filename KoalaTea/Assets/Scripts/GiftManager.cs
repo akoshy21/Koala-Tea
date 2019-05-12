95eae075263de38acb5f5b9b6cd95065f51aa5b4
Unknown <ak6429@nyu.edu>
2019-05-12 17:47:26 -0400

    story stuff and blinking

---
+++ KoalaTea/Assets/Scripts/GiftManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftManager : MonoBehaviour
+{
+    public BasicInkExample scnManager;
+
+    public GameObject abAdGift, abMumGift, rbAdGift, rbMumGift, mmhAdGift, mmhMumGift;
+
+    private void Start()
+    {
+
+        if (scnManager.auroraAd)
+        {
+            abAdGift.SetActive(true);
+        }
+        if (scnManager.auroraMum)
+        {
+            abMumGift.SetActive(true);
+        }
+        if (scnManager.rooibosAd)
+        {
+            abAdGift.SetActive(true);
+        }
+        if (scnManager.auroraAd)
+        {
+            abAdGift.SetActive(true);
+        }
+        if (scnManager.auroraAd)
+        {
+            abAdGift.SetActive(true);
+        }
+        if (scnManager.auroraAd)
+        {
+            abAdGift.SetActive(true);
+        }
+
+    }
+}
