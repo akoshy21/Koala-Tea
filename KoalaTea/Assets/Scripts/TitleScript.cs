a28611f5b80cd37d9e4c30ffe427d3a0e5874393
Unknown <asc656@nyu.edu>
2019-05-13 12:43:10 -0400

    some title screens stuff

--- KoalaTea/Assets/Scripts/TitleScript.cs
+++ KoalaTea/Assets/Scripts/TitleScript.cs
@@ -9,10 +9,14 @@ public class TitleScript : MonoBehaviour
     public RawImage character;
     public RawImage bg;
     private AudioSource audioS;
+    public Text title;
+    public Text playB;
 
     public List<Texture> chars = new List<Texture>();
     public List<Texture> bgs = new List<Texture>();
     public List<AudioClip> clips = new List<AudioClip>();
+    public List<Color> titleC = new List<Color>();
+    public List<Color> playC = new List<Color>();
 
     int rand;
 
@@ -24,13 +28,20 @@ public class TitleScript : MonoBehaviour
         character.GetComponent<RawImage>();
         bg.GetComponent<RawImage>();
         audioS = GetComponent<AudioSource>();
+        title = GetComponent<Text>();
+        playB = GetComponent<Text>();
 
         rand = Random.Range(0, 4);
 
         bg.texture = bgs[rand];
         character.texture = chars[rand];
+
         audioS.clip = clips[rand];
         audioS.Play();
+
+        title.color = titleC[rand];
+        playB.color = playC[rand];
+
 
         Debug.Log(chars[rand]);
 
