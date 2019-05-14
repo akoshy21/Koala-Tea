4bd05d2b4a578a36491d24f1eee294cef94121a6
Maddy <ms9642@nyu.edu>
2019-05-13 22:51:23 -0400

    basic drawing game kind of implemented

---
+++ KoalaTea/Assets/Scripts/toolControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (gameObject.name == "icon eraser")
+        {
+            paint.toolType = "eraser";
+            Debug.Log("eraser clicked");
+        }
+        if (gameObject.name == "icon pencil")
+        {
+            paint.toolType = "pencil";
+            Debug.Log("pencil clicked");
+        }
+    }
+}
