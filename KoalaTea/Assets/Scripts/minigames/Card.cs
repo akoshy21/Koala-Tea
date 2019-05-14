7e1fa792bda1e85e0cf2be0e783179a89a07dafc
Samina Trachier <st3225@nyu.edu>
2019-05-14 00:15:48 -0400

    created aurora blue minigame

---
+++ KoalaTea/Assets/Scripts/minigames/Card.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public static bool DO_NOT = false;
+
+    private int _state;
+    private int _cardValue;
+    private bool _initialized = false;
+
+    private Sprite _cardBack;
+    private Sprite _cardFace;
+
+    private GameObject _manager;
+
+    void Start()
+    {
+        _state = 1;
+        _manager = GameObject.FindGameObjectWithTag("Manager");
+        
+    }
+
+    public void setupGraphics()
+    {
+        _cardBack = GameManagerMini.instance.getCardBack();
+        _cardFace = GameManagerMini.instance.getCardFace(_cardValue);
+
+        flipCard();
+    }
+
+   public void flipCard()
+   {
+       if (_state == 0)
+       {
+           _state = 1;
+           Debug.Log("dskh");
+       }
+       else if (_state == 1)
+       {
+           _state = 0;
+           Debug.Log("kh");
+       }
+
+       if (_state == 0 & !DO_NOT)
+            gameObject.GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1 && !DO_NOT)
+            gameObject.GetComponent<Image>().sprite = _cardFace;
+    }
+
+    public int cardValue
+    {
+        get { return _cardValue; }
+        set { _cardValue = value; }
+    }
+
+    public int state
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    public bool initialized
+    {
+        get { return _initialized; }
+        set { _initialized = value; }
+    }
+
+    public void falseCheck()
+    {
+        StartCoroutine(pause());
+    }
+
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if (_state == 0)
+            GetComponent<Image>().sprite = _cardBack;
+        else if (_state == 1)
+            GetComponent<Image>().sprite = _cardFace;
+        DO_NOT = false;
+    }
+}
